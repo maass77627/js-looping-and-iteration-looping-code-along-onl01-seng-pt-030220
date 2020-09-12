9114060602fa1dda30e6ffd59c5874707b559441
kelly r maass <kmaass77627@gmail.com>
2020-09-12 02:48:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(array, event) {
-for (let i = 0, i < array.length, i++) {
+for (let i = 0; i < array.length; i++) {
   console.log(`Thank you #{array[i]} for the wonderful #{event} gift`);
 }
 
